547cace317fad9c731b69ed678cf9eb4ef93e908
taumechanica <eugen.steppenwolf@gmail.com>
2022-10-04 19:40:07 +0300

    Update dependencies

--- code/common/randint.spec.ts
+++ code/common/randint.spec.ts
@@ -1,7 +1,7 @@
 import { randint } from './randint';
 
 describe('RandInt', () => {
-    it('should random integer', () => {
+    it('should generate random integer', () => {
         for (let i = 0; i < 50; i++) {
             const r = randint(-10, 10);
             expect(r).toBeGreaterThanOrEqual(-10);
